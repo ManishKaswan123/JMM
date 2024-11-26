91884dc29648e5da40f33fb928cb836309476920
SatyamRana50 <194532@nith.ac.in>
2024-11-26 22:07:00 +0530

    aside menu abstracted

---
+++ src/sr/layout/components/aside/constant.ts
@@ -0,0 +1,141 @@
+// menuItems.js
+export const masterMenuItems = [
+  //   {
+  //     label: 'Business Category',
+  //     path: '/business-category',
+  //     iconPath: 'M9 12h6m2 4H7m12-8H5m16 8a9 9 0 11-18 0 9 9 0 0118 0z',
+  //   },
+  //   {
+  //     label: 'Category',
+  //     path: '/category',
+  //     iconPath:
+  //       'M3 7a1 1 0 011-1h16a1 1 0 011 1v3a1 1 0 01-1 1H4a1 1 0 01-1-1V7zM3 14a1 1 0 011-1h16a1 1 0 011 1v3a1 1 0 01-1 1H4a1 1 0 01-1-1v-3z',
+  //   },
+  //   {
+  //     label: 'Sub Category',
+  //     path: '/sub-category',
+  //     iconPath: 'M4 6h16M4 12h8m-8 6h16',
+  //   },
+  //   {
+  //     label: 'Invitation Coupon',
+  //     path: '/invitation-coupon',
+  //     iconPath:
+  //       'M9 11V9a2 2 0 114 0v2m-4 0a2 2 0 104 0m0 0v2a2 2 0 11-4 0v-2m0 0H6a2 2 0 00-2 2v4a2 2 0 002 2h12a2 2 0 002-2v-4a2 2 0 00-2-2h-3',
+  //   },
+  //   {
+  //     label: 'Seller Payment Plan',
+  //     path: '/seller-payment-plan',
+  //     iconPath:
+  //       'M6 12h12M6 16h12M6 8h12M6 20h12M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z',
+  //   },
+  //   {
+  //     label: 'Reward Point Plan',
+  //     path: '/reward-point-plan',
+  //     iconPath:
+  //       'M6 12h12M6 16h12M6 8h12M6 20h12M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z',
+  //   },
+  //   {
+  //     label: 'Reward Earning',
+  //     path: '/reward-earning',
+  //     iconPath:
+  //       'M6 12h12M6 16h12M6 8h12M6 20h12M4 4h16a2 2 0 012 2v12a2 2 0 01-2 2H4a2 2 0 01-2-2V6a2 2 0 012-2z',
+  //   },
+  //   {
+  //     label: 'Reward Transaction',
+  //     path: '/reward-uses-history',
+  //     iconPath: 'M9 12h6m2 4H7m12-8H5m16 8a9 9 0 11-18 0 9 9 0 0118 0z',
+  //   },
+  //   {
+  //     label: 'Mobile App Version History',
+  //     path: '/mobile-app-version-history',
+  //     iconPath: 'M12 8v4l3 3M7 4h10a2 2 0 012 2v12a2 2 0 01-2 2H7a2 2 0 01-2-2V6a2 2 0 012-2z',
+  //   },
+  // Add other menu items here
+]
+export const normalMenuItems = [
+  //   {
+  //     label: 'Dashboard',
+  //     path: '/dashboard',
+  //     iconPath:
+  //       'M9 2H7a2 2 0 00-2 2v16a2 2 0 002 2h10a2 2 0 002-2V4a2 2 0 00-2-2h-2m-4 0v4m0-4H9m0 0H7',
+  //   },
+  //   {
+  //     label: 'Users',
+  //     path: '/users',
+  //     iconPath: 'M12 14c-3.866 0-7 3.134-7 7m14 0c0-3.866-3.134-7-7-7m0-6a3 3 0 100-6 3 3 0 000 6z',
+  //   },
+  //   {
+  //     label: 'Product',
+  //     path: '/product',
+  //     iconPath:
+  //       'M3 8h18M3 12h18M3 16h18M5 4h14a1 1 0 011 1v14a1 1 0 01-1 1H5a1 1 0 01-1-1V5a1 1 0 011-1z',
+  //   },
+  //   {
+  //     label: 'Order',
+  //     path: '/order',
+  //     iconPath: 'M9 12h6m2 4H7m12-8H5m16 8a9 9 0 11-18 0 9 9 0 0118 0z',
+  //   },
+  //   {
+  //     label: 'Transaction',
+  //     path: '/transactions',
+  //     iconPath:
+  //       'M5 11h14M5 15h14M5 19h14M3 5h18a1 1 0 011 1v14a1 1 0 01-1 1H3a1 1 0 01-1-1V6a1 1 0 011-1z',
+  //   },
+  //   {
+  //     label: 'Eighty Six',
+  //     path: '/86',
+  //     iconPath:
+  //       'M9 12a3 3 0 100-6 3 3 0 000 6zM15 12a3 3 0 100-6 3 3 0 000 6zM9 12a3 3 0 100 6 3 3 0 000-6zM15 12a3 3 0 100 6 3 3 0 000-6z',
+  //   },
+  //   {
+  //     label: 'Eighty Six Response',
+  //     path: '/86-response',
+  //     iconPath:
+  //       'M9 12a3 3 0 100-6 3 3 0 000 6zM15 12a3 3 0 100-6 3 3 0 000 6zM9 12a3 3 0 100 6 3 3 0 000-6zM15 12a3 3 0 100 6 3 3 0 000-6z',
+  //   },
+  //   {
+  //     label: 'Chats',
+  //     path: '/chats',
+  //     iconPath: 'M21 15a2 2 0 01-2 2H7l-4 4V5a2 2 0 012-2h12a2 2 0 012 2z',
+  //   },
+
+  {
+    label: 'Jobs',
+    path: '/jobs',
+    iconPath:
+      'M17 9V7a4 4 0 00-8 0v2M5 20h14a2 2 0 002-2v-5a2 2 0 00-2-2H5a2 2 0 00-2 2v5a2 2 0 002 2z',
+  },
+  {
+    label: 'Cleaner',
+    path: '/cleaner',
+    iconPath:
+      'M16.5 4.5l3 3m-2.121-2.121L8.25 16.5m5.25-11.25l-2.121 2.121M9.5 20.5H6l1.5-4.5 7.121-7.121 2.121 2.121L9.5 20.5z',
+  },
+  {
+    label: 'Task List',
+    path: '/tasklist',
+    iconPath: 'M5 13l4 4L19 7M5 3v6m0 0h6',
+  },
+  {
+    label: 'Company',
+    path: '/company',
+    iconPath: 'M4 6h16M4 10h16M10 14h10M4 14h4v7H4z',
+  },
+  {
+    label: 'Work Order',
+    path: '/workorder',
+    iconPath: 'M9 17v-2a4 4 0 00-8 0v2h8zm13 0v-2a4 4 0 00-8 0v2h8zm-13 4h8m-8-2h8m0-2H6',
+  },
+  {
+    label: 'Task',
+    path: '/task',
+    iconPath: 'M8 7h8M8 11h8M8 15h8M8 19h8',
+  },
+  {
+    label: 'Proposal Details',
+    path: '/proposaldetails',
+    iconPath: 'M6 5h12M6 10h12M6 15h8M6 20h4',
+  },
+
+  // Add other menu items here
+]
