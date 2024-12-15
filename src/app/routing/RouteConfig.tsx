a4779109534111932ae1f513e7009ebfe9f6325c
SatyamRana50 <194532@nith.ac.in>
2024-12-15 17:20:36 +0530

    private routes abstracted

---
+++ src/app/routing/RouteConfig.tsx
@@ -0,0 +1,44 @@
+import {lazy} from 'react'
+
+const CleanerDetailsCard = lazy(() => import('app/pages/module/cleaner/CleanerDetails'))
+const UserDetailCard = lazy(() => import('app/pages/module/user/UserDetail'))
+const JobDetailsCard = lazy(() => import('app/pages/module/jobs/JobDetails'))
+const Address = lazy(() => import('app/pages/module/address/Address'))
+const BusinessCategory = lazy(() => import('app/pages/module/masterData/business/Business'))
+const User = lazy(() => import('app/pages/module/user/User'))
+const Jobs = lazy(() => import('app/pages/module/jobs/Jobs'))
+const WorkOrder = lazy(() => import('app/pages/module/workOrder/WorkOrder'))
+const Cleaner = lazy(() => import('app/pages/module/cleaner/Cleaner'))
+const TaskList = lazy(() => import('app/pages/module/taskList/TaskList'))
+const Company = lazy(() => import('app/pages/module/company/Company'))
+const CompanyDetail = lazy(() => import('app/pages/module/company/CompanyDetail'))
+const Task = lazy(() => import('app/pages/module/task/Task'))
+const ProposalDetails = lazy(() => import('app/pages/module/proposalDetails/ProposalDetails'))
+const Contractor = lazy(() => import('app/pages/module/contractor/Contractor'))
+const Messages = lazy(() => import('app/pages/module/messages/Messages'))
+const Notes = lazy(() => import('app/pages/module/notes/Notes'))
+const Supervisor = lazy(() => import('app/pages/module/supervisor/Supervisor'))
+const Application = lazy(() => import('app/pages/module/applications/Application'))
+
+export const routeConfigs = [
+  {path: '/business-category', element: <BusinessCategory />},
+  {path: '/jobs', element: <Jobs />},
+  {path: '/job/:id', element: <JobDetailsCard />},
+  {path: '/workorder', element: <WorkOrder />},
+  {path: '/cleaner', element: <Cleaner />},
+  {path: '/cleaner/:id', element: <CleanerDetailsCard />},
+  {path: '/tasklist', element: <TaskList />},
+  {path: '/company', element: <Company />},
+  {path: '/company/:id', element: <CompanyDetail />},
+  {path: '/task', element: <Task />},
+  {path: '/proposaldetails', element: <ProposalDetails />},
+  {path: '/contractor', element: <Contractor />},
+  {path: '/users', element: <User />},
+  {path: '/user/details/:userId', element: <UserDetailCard />},
+  {path: '/notes', element: <Notes />},
+  {path: '/message', element: <Messages />},
+  {path: '/supervisor', element: <Supervisor />},
+  {path: '/address', element: <Address />},
+  {path: '/user/address/:userId', element: <Address />},
+  {path: '/application', element: <Application />},
+]
