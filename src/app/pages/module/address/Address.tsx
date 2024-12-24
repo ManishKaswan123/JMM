db1ec2855df1abff03ab8f598f564db60c8a7576
SatyamRana50 <194532@nith.ac.in>
2024-12-24 16:25:12 +0530

    create and updated added to address

--- src/app/pages/module/address/Address.tsx
+++ src/app/pages/module/address/Address.tsx
@@ -1,12 +1,19 @@
 import React, {useState, useEffect, useMemo, useCallback} from 'react'
 import Pagination from 'sr/helpers/ui-components/dashboardComponents/Pagination'
 import DashboardWrapper from 'app/pages/dashboard/DashboardWrapper'
-import {AiOutlineClose, AiOutlineFilter} from 'react-icons/ai'
+import {AiOutlineClose, AiOutlineFilter, AiOutlinePlus} from 'react-icons/ai'
 import {Button} from 'sr/helpers'
 import Filter from 'sr/helpers/ui-components/Filter'
 import {useQuery} from '@tanstack/react-query'
 import PaginationSkeleton from 'sr/helpers/ui-components/dashboardComponents/PaginationSkeleton'
-import {Address, AddressApiResponse, fetchAddress} from 'sr/utils/api/addressApi'
+import {
+  Address,
+  AddressApiResponse,
+  AddressData,
+  fetchAddress,
+  useCreateAddress,
+  useUpdateAddress,
+} from 'sr/utils/api/addressApi'
 import AddressTable from './AddressTable'
 import {AddressDetailsCard} from './AddressDetails'
 import {RootState} from 'sr/redux/store'
@@ -15,24 +22,58 @@ import {useActions} from 'sr/utils/helpers/useActions'
 import {FieldsArray} from 'sr/constants/fields'
 import {useParams} from 'react-router-dom'
 import SkeletonTable from 'sr/helpers/ui-components/SkeletonTable'
+import DynamicModal from 'sr/helpers/ui-components/DynamicPopUpModal'
 
 interface AddressFilters {
   limit?: number
   page?: number
   individual_id?: string
+  no_of_rooms?: number
+  no_of_bath?: number
+  total_area?: number
+  status?: string
+}
+
+interface AddressCreatePayload {
+  no_of_rooms: number
+  no_of_bath: number
+  total_area: number
+  individual_id: string
+  remark: string
+  address: Record<string, string | number>
+  address_type: string
+  status: string
+}
+
+interface AddressFormPayload extends Omit<AddressCreatePayload, 'address'> {
+  address_line_1: string
+  address_line_2: string
+  country: string
+  city: string
+  state: string
+  postal: number
+  lat: number
+  lng: number
+}
+interface AddressUpdatePayload extends AddressCreatePayload {
+  id: string
 }
 
 const Custom: React.FC = () => {
   const {userId} = useParams<{userId: string | undefined}>()
   const [currentPage, setCurrentPage] = useState<number>(1)
+  const [selectedData, setSelectedData] = useState<AddressData>()
   const [selectedAddress, setSelectedAddress] = useState<Address | undefined>()
   const [filters, setFilters] = useState<AddressFilters>({individual_id: userId})
   const [isFilterVisible, setIsFilterVisible] = useState<boolean>(false)
   const [itemsPerPage, setItemsPerPage] = useState<number>(8)
+  const [isCreateModalOpen, setIsCreateModalOpen] = useState<boolean>(false)
   const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false)
   const userData = useSelector((state: RootState) => state.user.userData)
   const userStatus = useSelector((state: RootState) => state.user.status)
   const {fetchUserData} = useActions()
+  const createMutation = useCreateAddress()
+  const updateMutation = useUpdateAddress()
 
   const fields: FieldsArray = useMemo(
     () => [
@@ -68,6 +109,7 @@ const Custom: React.FC = () => {
         name: [
           {name: 'Active', id: 'active'},
           {name: 'Inactive', id: 'inactive'},
+          {name: 'Deleted', id: 'deleted'},
         ],
         topLabel: 'Status',
         placeholder: 'Select Status',
@@ -76,8 +118,131 @@ const Custom: React.FC = () => {
     ],
     [userData]
   )
+  const createFields: FieldsArray = useMemo(
+    () => [
+      {
+        type: 'dropdown',
+        label: 'individual_id',
+        name: userData,
+        topLabel: 'Individual',
+        placeholder: 'Select Individual',
+        labelKey: 'name',
+        required: true,
+      },
+      {
+        type: 'dropdown',
+        label: 'address_type',
+        name: [
+          {
+            name: 'Hotel',
+            id: 'hotel',
+          },
+          {
+            name: 'Residencial',
+            id: 'Residencial',
+          },
+          {
+            name: 'Commercial',
+            id: 'Commercial',
+          },
+          {
+            name: 'Industrial',
+            id: 'Industrial',
+          },
+        ],
+        topLabel: 'Address Type',
+        placeholder: 'Select Address Type',
+        labelKey: 'name',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'Address Line 1',
+        name: 'address_line_1',
+        placeholder: 'Address Line 1',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'Address Line 2',
+        name: 'address_line_2',
+        placeholder: 'Address Line 2',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'Country',
+        name: 'country',
+        placeholder: 'country',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'City',
+        name: 'city',
+        placeholder: 'City',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'State',
+        name: 'state',
+        placeholder: 'State',
+        required: true,
+      },
+      {
+        type: 'number',
+        label: 'Postal Code',
+        name: 'postal',
+        placeholder: 'Postal Code',
+        required: true,
+      },
+      {
+        type: 'number',
+        label: 'Rooms Count',
+        name: 'no_of_rooms',
+        placeholder: 'No. of Rooms',
+        required: true,
+      },
+      {
+        type: 'number',
+        label: 'Bathrooms Count',
+        name: 'no_of_bath',
+        placeholder: 'No. of Bathrooms',
+        required: true,
+      },
+      {
+        type: 'number',
+        label: 'Total Area',
+        name: 'total_area',
+        placeholder: 'Total Area',
+        required: true,
+      },
+      {
+        type: 'dropdown',
+        label: 'status',
+        name: [
+          {name: 'Active', id: 'active'},
+          {name: 'Inactive', id: 'inactive'},
+          {name: 'Deleted', id: 'deleted'},
+        ],
+        topLabel: 'Status',
+        placeholder: 'Select Status',
+        labelKey: 'name',
+        required: true,
+      },
+      {
+        type: 'text',
+        label: 'Remarks',
+        name: 'remark',
+        placeholder: 'Remarks',
+        required: true,
+      },
+    ],
+    [userData]
+  )
 
-  const {data, error, isLoading, isError, refetch} = useQuery<AddressApiResponse>({
+  const {data, isLoading, isError} = useQuery<AddressApiResponse>({
     queryKey: ['address', {limit: itemsPerPage, page: currentPage, ...filters}],
     queryFn: async () => fetchAddress({limit: itemsPerPage, page: currentPage, ...filters}),
     // placeholderData: keepPreviousData,
@@ -106,17 +271,79 @@ const Custom: React.FC = () => {
     setCurrentPage(1)
     setIsFilterVisible(false) // Hide filter after applying
   }
-  // const handleUpdateUser = async (payload: UserInterface) => {
-  //   setIsUpdateModalOpen(false)
-  //   if (selectedUserForUpdate) {
-  //     const userId = selectedUserForUpdate.id
-  //     const res = await updateUser(userId, payload)
-  //     if (!res) {
-  //       return
-  //     }
-  //     refetch()
-  //   }
-  // }
+  const handleCreateAddress = async (payload: AddressFormPayload) => {
+    const data: AddressCreatePayload = {
+      no_of_rooms: payload.no_of_rooms,
+      no_of_bath: payload.no_of_bath,
+      total_area: payload.total_area,
+      individual_id: payload.individual_id,
+      remark: payload.remark,
+      address: {
+        address_line_1: payload.address_line_1,
+        address_line_2: payload.address_line_2,
+        country: payload.country,
+        city: payload.city,
+        state: payload.state,
+        postal: payload.postal,
+        lat: 0,
+        lng: 0,
+      },
+      address_type: payload.address_type,
+      status: payload.status,
+    }
+    setIsCreateModalOpen(false)
+    createMutation.mutate(data)
+  }
+  const handleEditAddress = async (payload: AddressFormPayload) => {
+    if (!selectedData) {
+      setIsUpdateModalOpen(false)
+      return
+    }
+    setIsUpdateModalOpen(false)
+    const data: AddressUpdatePayload = {
+      no_of_rooms: payload.no_of_rooms,
+      no_of_bath: payload.no_of_bath,
+      total_area: payload.total_area,
+      individual_id: payload.individual_id,
+      remark: payload.remark,
+      address: {
+        address_line_1: payload.address_line_1,
+        address_line_2: payload.address_line_2,
+        country: payload.country,
+        city: payload.city,
+        state: payload.state,
+        postal: payload.postal,
+        lat: 0,
+        lng: 0,
+      },
+      address_type: payload.address_type,
+      status: payload.status,
+      id: selectedData.id,
+    }
+    updateMutation.mutate({payload: data})
+  }
+  const defaultValues: AddressFormPayload | undefined = useMemo(() => {
+    if (!selectedData) return undefined
+    return {
+      address_line_1: selectedData.address.address_line_1,
+      address_line_2: selectedData.address.address_line_2,
+      country: selectedData.address.country,
+      city: selectedData.address.city,
+      state: selectedData.address.state,
+      postal: selectedData.address.postal,
+      lat: selectedData.address.lat,
+      lng: selectedData.address.lng,
+      no_of_rooms: selectedData.no_of_rooms,
+      no_of_bath: selectedData.no_of_bath,
+      total_area: selectedData.total_area,
+      individual_id: selectedData.individual_id._id,
+      address_type: selectedData.address_type,
+      status: selectedData.status,
+      remark: selectedData.remark,
+
+      id: selectedData.id,
+    }
+  }, [selectedData])
 
   if (selectedAddress) {
     return (
@@ -136,14 +363,12 @@ const Custom: React.FC = () => {
               Address
             </h2>
             <div className='flex items-center'>
-              {/* <Button
-                    label='Update Reward Point Plan'
-                    Icon={AiOutlineReload}
-                    onClick={() => {
-                      setIsCreateModalOpen(true)
-                    }}
-                    className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full shadow-md inline-flex items-center mb-2 sm:mb-0 sm:mr-3'
-                  ></Button> */}
+              <Button
+                label='Create new'
+                Icon={AiOutlinePlus}
+                onClick={() => setIsCreateModalOpen(true)}
+                className='bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded-full shadow-md inline-flex items-center mb-2 sm:mb-0 sm:mr-3'
+              ></Button>
               <Button
                 label={`${isFilterVisible ? 'Close' : 'Filters'}`}
                 Icon={!isFilterVisible ? AiOutlineFilter : AiOutlineClose}
@@ -181,6 +406,7 @@ const Custom: React.FC = () => {
               addressData={data?.data}
               onSelectAddress={setSelectedAddress}
               setIsUpdateModalOpen={setIsUpdateModalOpen}
+              setSelectedData={setSelectedData}
             />
           )}
         </div>
@@ -201,16 +427,25 @@ const Custom: React.FC = () => {
           />
         )}
       </div>
-      {/* {isUpdateModalOpen && selectedUserForUpdate && (
+      {isCreateModalOpen && (
         <DynamicModal
-          label='Update Individual'
+          label='Create Address'
+          isOpen={isCreateModalOpen}
+          onClose={() => setIsCreateModalOpen(false)}
+          fields={createFields}
+          onSubmit={handleCreateAddress}
+        />
+      )}
+      {isUpdateModalOpen && defaultValues && (
+        <DynamicModal
+          label='Update Address'
           isOpen={isUpdateModalOpen}
           onClose={() => setIsUpdateModalOpen(false)}
-          fields={updateFields}
-          defaultValues={selectedUserForUpdate}
-          onSubmit={handleUpdateUser}
+          fields={createFields}
+          defaultValues={defaultValues}
+          onSubmit={handleEditAddress}
         />
-      )} */}
+      )}
     </>
   )
 }
--- src/app/pages/module/address/AddressTable.tsx
+++ src/app/pages/module/address/AddressTable.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {FaEye} from 'react-icons/fa'
+import {FaEdit, FaEye} from 'react-icons/fa'
 import {Address, AddressData} from 'sr/utils/api/addressApi'
 
 interface AddressTableProps {
   addressData: AddressData[] | undefined
   onSelectAddress: React.Dispatch<React.SetStateAction<Address | undefined>>
   setIsUpdateModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+  setSelectedData: React.Dispatch<React.SetStateAction<AddressData | undefined>>
 }
 
 const AddressTable: React.FC<AddressTableProps> = (props) => {
@@ -34,7 +35,7 @@ const AddressTable: React.FC<AddressTableProps> = (props) => {
                 Status
               </th>
               <th className='px-5 py-3 bg-gray-200 text-left text-xs font-semibold text-gray-800 uppercase tracking-wider'>
-                View Address
+                Actions
               </th>
             </tr>
           </thead>
@@ -56,16 +57,35 @@ const AddressTable: React.FC<AddressTableProps> = (props) => {
                 <td className='px-5 py-5 text-left border-b border-gray-200 text-sm'>
                   <p>{address.remark}</p>
                 </td>
-                <td className='px-5 py-5 text-left border-b border-gray-200 text-sm'>
-                  <p>{address.status}</p>
+                <td className='px-5 py-5 border-b border-gray-200 text-sm'>
+                  <p
+                    className={`whitespace-no-wrap ${
+                      address.status === 'active'
+                        ? 'text-green-500'
+                        : address.status === 'inactive'
+                        ? 'text-red-500'
+                        : 'text-gray-500' // Default color for unknown statuses
+                    }`}
+                  >
+                    {address.status}
+                  </p>
                 </td>
                 <td className='px-5 py-5 border-b border-gray-200 text-sm'>
-                  <FaEye
-                    className='text-blue-500 cursor-pointer mr-4 h-4 w-4'
-                    onClick={() => {
-                      props.onSelectAddress(address.address)
-                    }}
-                  />
+                  <div className='flex'>
+                    <FaEdit
+                      className='text-blue-500 cursor-pointer mr-4 h-4 w-4'
+                      onClick={() => {
+                        props.setSelectedData(address)
+                        props.setIsUpdateModalOpen(true)
+                      }}
+                    />
+                    <FaEye
+                      className='text-blue-500 cursor-pointer mr-4 h-4 w-4'
+                      onClick={() => {
+                        props.onSelectAddress(address.address)
+                      }}
+                    />
+                  </div>
                 </td>
               </tr>
             ))}
